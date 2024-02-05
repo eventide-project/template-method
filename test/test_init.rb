0e092adf7e21e2bc8b01fba6ea4d3e644bb7ebf7
Alexander Repnikov <aleksander.repnikov@gmail.com>
2024-02-05 17:50:31 +0100

    TemplateMethod gem Scaffold

---
+++ test/test_init.rb
@@ -0,0 +1,10 @@
+ENV["TEST_BENCH_DETAIL"] ||= ENV["D"]
+
+puts RUBY_DESCRIPTION
+
+require_relative '../init.rb'
+require 'template_method/controls'
+
+require 'pp'
+
+require 'test_bench'; TestBench.activate
